b8b2268726598689dab2b12d6b1bb1d3a7a72218
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2024-07-19 02:42:22 +0200

    Add plugin structure

---
+++ plugin/src/main/java/fr/flowsqy/eventannouncer/EventAnnouncerPlugin.java
@@ -0,0 +1,12 @@
+package fr.flowsqy.eventannouncer;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class EventAnnouncerPlugin extends Plugin {
+
+	@Override
+	public void onEnable() {
+
+	}
+ 
+}
